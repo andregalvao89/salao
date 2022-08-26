248083abba8de45fe6e95ad8841b97cf28fafabc
André Galvão <andregalvao89@gmail.com>
2022-08-26 10:22:11 -0300

    Erro nas dependencias de validacao

--- src/main/java/com/salao/dto/ClienteDTO.java
+++ src/main/java/com/salao/dto/ClienteDTO.java
@@ -9,6 +9,12 @@ import lombok.Setter;
 import javax.persistence.Entity;
 import java.io.Serializable;
 
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 @Getter
 @AllArgsConstructor
 @Setter
@@ -17,6 +23,8 @@ public class ClienteDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Long id;
+
+    @NotEmpty(message = "Preenchimento obrigatório")
     private String nome;
     private String email;
 
